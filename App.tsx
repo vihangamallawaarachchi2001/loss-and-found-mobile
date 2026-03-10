750703608161ce4de2304b19845bc8ee4e67546a
vihanga <vihanganethusara00@gmail.com>
2026-03-11 04:19:15 +0530

    Refactor App component to use SafeAreaProvider and AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "./global.css"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+	 <SafeAreaProvider>
+		 <AppNavigator />
+	 </SafeAreaProvider>
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
